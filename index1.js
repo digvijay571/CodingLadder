31b3a8898f0beffe1f29e4b8efc1f7daf0545523
digvijay571 <digvjay18012001@gmail.com>
2020-09-06 22:16:18 -0700

    j

--- index1.js
+++ index1.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
       mouseleave:function(){
          $('.dropdown_1').addClass('hide').animate({
             opacity:0.1,
-            top:"0%"
+            top:"-5%"
          })
       $('.i2>a')[0].style.color ="rgb(142,116,116)"
    },
