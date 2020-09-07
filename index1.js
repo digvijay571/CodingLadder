ab596580a3078c9bb0f2c519f2239316c99c735b
digvijay571 <digvjay18012001@gmail.com>
2020-09-06 22:10:10 -0700

    b

--- index1.js
+++ index1.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
       mouseenter:function(){
          $('.dropdown_1').removeClass('hide').animate({
             opacity:1,
-            top:"50%"
+            top:"10%"
          })
          $('.i2>a')[0].style.color ="red"   
       },
       mouseleave:function(){
          $('.dropdown_1').addClass('hide').animate({
             opacity:0.1,
-            top:"70%"
+            top:"20%"
          })
       $('.i2>a')[0].style.color ="rgb(142,116,116)"
    },
